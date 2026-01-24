a165892efccbb1e7af36eeed786eb893c60cce40
KRSMain <qnorton.krs@gmail.com>
2026-01-24 14:54:17 -0700

    Adding a file scanner
    Scans a folder, outputting a list of all items and their size. Useful for some debugging purposes.

---
+++ scanner.ps1
@@ -0,0 +1,22 @@
+$path = "ENTER YOUR FILENAME HERE"
+
+$items = Get-ChildItem -Path $path -Recurse -Force
+
+$results = foreach ($item in $items) {
+    if ($item.PSIsContainer) {
+        $size = (Get-ChildItem -Path $item.FullName -Recurse -File -Force |
+                 Measure-Object Length -Sum).Sum
+    }
+    else {
+        $size = $item.Length
+    }
+
+    [PSCustomObject]@{
+        Type     = if ($item.PSIsContainer) { "Directory" } else { "File" }
+        FullName = $item.FullName
+        SizeBytes = $size
+        SizeMB    = "{0:N2}" -f ($size / 1MB)
+    }
+}
+
+$results | Sort-Object SizeBytes -Descending | Select-Object Type, FullName, SizeMB
